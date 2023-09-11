b8de8d1bd8970c994e55e7d901ad04d22084f3a0
Paul Kolesa <maxxdoutvdub@gmail.com>
2023-09-11 16:15:41 -0700

    Added some JS animations, favicon, and web app site manifest.

--- static/js/cm.js
+++ static/js/cm.js
@@ -1,7 +1,18 @@
 var opacity = 0;
 var intervalID = 0;
 var out = 0;
-window.onload = fadeBounce;
+var hello = [
+	['GREETINGS PROFESSOR FALKEN.', 'tOut0'],
+	['HOW ARE YOU FEELING TODAY?', 'tOut1'],
+	['SHALL WE PLAY A GAME?', 'tOut2']
+]
+var eol = 'sOut';
+var eolChar = '.';
+var elementId = 0;
+var i = 0;
+var tSpeed = 100;
+
+window.onload = main;
           
 function fadeBounce() {
   setInterval(show, 200);
@@ -20,4 +31,37 @@ function show() {
     opacity += 0.1;
   }
   body.style.opacity = opacity;
+}
+
+function blink(element) {
+	var speed = 1000;
+	var eId = document.getElementById(element);
+	eId.innerHTML += eolChar;
+	setInterval(function (eId) {
+		eId.style.visibility = (eId.style.visibility == 'hidden' ? '' : 'hidden');
+	}, speed, eId);
+}
+
+function terminal(msgOut) {
+	var msg = msgOut[elementId][0];
+	var eId = msgOut[elementId][1];
+	var msgLen = msg.length;
+	if (i < msg.length) {
+		document.getElementById(eId).innerHTML += msg.charAt(i);
+		i++;
+	} else {
+		i = 0;
+		elementId++;
+	}
+	if (elementId < hello.length) {
+		setTimeout(terminal, tSpeed, msgOut, elementId);	
+	}
+	if (elementId === hello.length) {
+		blink(eol);
+	}
+}
+
+function main() {
+	fadeBounce();
+	terminal(hello);		
 }
